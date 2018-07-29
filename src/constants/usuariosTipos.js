2ed01e752e91718083c6272332321bd9c4230b07
Andre Rosa <andre.i.r@live.com>
2018-07-29 18:16:24 -0300

    adjustments

--- src/constants/usuariosTipos.js
+++
@@ -1,7 +0,0 @@
-export const USUARIOS_TIPOS = {
-    ADMINISTRADOR: {id: 1, nome: "Administrador"},
-    MEDICO: {id: 2, nome: "Médico"},
-    COACH: {id: 3, nome: "Coach"},
-    CLASSIFICADOR: {id: 4, nome: "Classificador"},
-    DIGITADOR: {id: 5, nome: "Digitador"}
-};
